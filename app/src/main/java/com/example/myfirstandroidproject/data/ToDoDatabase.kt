7f0d785e07bbf5a45fa103cca4d6f9ae21536169
billyyan <By869422>
2023-04-22 20:08:12 -0700

    Setup Room Database

---
+++ app/src/main/java/com/example/myfirstandroidproject/data/ToDoDatabase.kt
@@ -0,0 +1,11 @@
+package com.example.myfirstandroidproject.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.myfirstandroidproject.data.models.ToDoTask
+
+@Database(entities = [ToDoTask::class], version = 1, exportSchema = false)
+abstract class ToDoDatabase: RoomDatabase() {
+
+    abstract fun toDoDao(): ToDoDao
+}
