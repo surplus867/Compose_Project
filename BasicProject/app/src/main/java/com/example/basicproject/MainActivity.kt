f437d3da748d8981dd40f044011c3ed9bfdb65f2
billyyan <By869422>
2023-09-07 16:32:11 -0700

    Box

--- BasicProject/app/src/main/java/com/example/basicproject/MainActivity.kt
+++ BasicProject/app/src/main/java/com/example/basicproject/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
@@ -12,14 +13,18 @@ import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.basicproject.ui.theme.BasicProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,16 +34,7 @@ class MainActivity : ComponentActivity() {
             BasicProjectTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colorScheme.background) {
-                    Row(
-                        modifier = Modifier
-                            .height(500.dp)
-                            .width(500.dp)
-                            .background(Color.LightGray),
-                        horizontalArrangement = Arrangement.Start
-                    ) {
-                        CustomItem(weight = 3f, color = MaterialTheme.colorScheme.secondary)
-                        CustomItem(weight = 1f)
-                    }
+                    Greeting()
                 }
             }
         }
@@ -46,26 +42,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun RowScope.CustomItem(weight: Float, color: Color = MaterialTheme.colorScheme.primary) {
-    Surface(
-        modifier = Modifier
-            .width(200.dp)
-            .weight(weight),
-        color = color
-    ) {}
+fun Greeting() {
+    Box(modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.TopCenter
+        ) {
+        Box(modifier = Modifier
+            .background(Color.Blue),
+            contentAlignment = Alignment.Center
+        ) {
+            Box(modifier = Modifier.height(50.dp).width(50.dp).background(Color.Green))
+            Text(text = "I Love Android!", fontSize = 40.sp)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BasicProjectTheme {
-        Row(
-            modifier = Modifier.fillMaxSize(),
-            horizontalArrangement = Arrangement.Start,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            CustomItem(weight = 3f, color = MaterialTheme.colorScheme.secondary)
-            CustomItem(weight = 1f)
-        }
+        Greeting()
     }
 }
