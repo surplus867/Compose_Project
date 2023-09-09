9b1a6db7e2e05a0d378064acfab3d53c458f50fe
billyyan <By869422>
2023-09-08 23:40:45 -0700

    Text Customization

--- BasicProject/app/src/main/java/com/example/basicproject/MainActivity.kt
+++ BasicProject/app/src/main/java/com/example/basicproject/MainActivity.kt
@@ -4,24 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.ParagraphStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -34,7 +35,15 @@ class MainActivity : ComponentActivity() {
             BasicProjectTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colorScheme.background) {
-                    Greeting()
+                    Column(modifier = Modifier.fillMaxSize()) {
+                        Text(
+                            text = stringResource(id = R.string.app_name),
+                            modifier = Modifier
+                                .background(MaterialTheme.colorScheme.primary)
+                                .padding(16.dp)
+                        )
+                    }
+
                 }
             }
         }
@@ -42,24 +51,54 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting() {
-    Box(modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.TopCenter
-        ) {
-        Box(modifier = Modifier
-            .background(Color.Blue),
-            contentAlignment = Alignment.Center
-        ) {
-            Box(modifier = Modifier.height(50.dp).width(50.dp).background(Color.Green))
-            Text(text = "I Love Android!", fontSize = 40.sp)
-        }
-    }
+fun CustomText() {
+    Text(
+        text = stringResource(id = R.string.app_name),
+        modifier = Modifier
+            // order execution from top to bottom
+            .background(MaterialTheme.colorScheme.primary)
+            .padding(16.dp)
+            .width(200.dp),
+        color = Color.White,
+        fontSize = MaterialTheme.typography.headlineMedium.fontSize,
+        fontStyle = FontStyle.Italic,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center
+    )
+}
+
+@Composable
+fun CustomText2() {
+    Text(
+        text = buildAnnotatedString {
+            withStyle(style = ParagraphStyle(textAlign = TextAlign.Center)) {
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.Blue,//Set the color directly
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                ) {
+                    append("A")
+                }
+                append("BCD")// Combine "B," "C," and "D" into a single append
+            }
+        },
+        modifier = Modifier.width(200.dp)
+    )
+}
+
+@Composable
+fun CustomText3(){
+    Text(text = "Hello World!".repeat(20), maxLines = 2, overflow = TextOverflow.Ellipsis)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     BasicProjectTheme {
-        Greeting()
+        Column(modifier = Modifier.fillMaxSize()) {
+            CustomText3()
+        }
     }
 }
